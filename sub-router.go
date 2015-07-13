4b8b601145594312957479c305b730041d3881cb
A Darian <git@darian.af>
2015-07-13 15:44:57 +0100

    adding some licensing stuff and the beginnings of documentation

--- sub-router.go
+++ sub-router.go
@@ -4,6 +4,15 @@
 
 package forest
 
+/*
+SubRouter is the basic building block of forest applications. In the main.go
+file where a service is initialized, app.RegisterRoute is called with a path
+string and a SubRouter instance in order to logically group all of the endpoints
+a particular service will answer. Since each App instance exposes its underlying
+bear.Mux (via (*App).Router), it is not strictly necessary to use SubRouter at
+all, it simply exists to prove a convenient way to instantiate many endponits in
+one place.
+*/
 type SubRouter interface {
 	Route(path string)
 }
